806a38934a0ae053b6e33087151653fea801ada9
Jinhyung <thswlsgud0423@gmail.com>
2024-12-21 18:28:56 +0100

    homepage is added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from "./pages/homepage";
+import MyCourses from "./pages/mycourses";
 
 const App = () => {
   return (
   <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/mycourses" element={<MyCourses />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -1,11 +1,54 @@
-import "./homepage.css"
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./homepage.css";
 
-export default function HomePage() {
+const Homepage: React.FC = () => {
   return (
-    <div className="gex">
-        <div className="gex2">
-          tesEYTSHDFHSDFt
-        </div >
-    </div>
+    <>
+      {/* Navbar */}
+      <Navbar bg="light" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand href="/">Course Review</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="mycourse.html">My Courses</Nav.Link>
+              <NavDropdown title="Studies" id="studies-dropdown">
+                <NavDropdown.Item href="cs.html">CS</NavDropdown.Item>
+                <NavDropdown.Item href="ai.html">AI</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="About" id="about-dropdown">
+                <NavDropdown.Item href="aboutjku.html">About JKU</NavDropdown.Item>
+                <NavDropdown.Item href="aboutus.html">About Us</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="contact.html">Contact</Nav.Link>
+            </Nav>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search for a course..."
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Main Content */}
+      <div className="content mt-5 pt-5">
+        <h1>We going to da MOON</h1>
+      </div>
+    </>
   );
-}
+};
+
+export default Homepage;
